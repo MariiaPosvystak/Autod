f00dc2fa578f6ab7534df366bdc9168d2f48dd71
opilane <opilane@HP-CZC2349HT4>
2025-12-03 18:20:30 +0200

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,20 +35,36 @@
             Lisavorm_oma_btn = new Button();
             Vaata_oma_btn = new Button();
             Autod = new TabPage();
+            Kust_auto_btm = new Button();
+            Lisavorm_auto_btn = new Button();
+            Vaata_auto_btn = new Button();
             HjT = new TabPage();
             dataGridView_omanik = new DataGridView();
             Omanikud = new Label();
             oma_nimi_txt = new TextBox();
-            label2 = new Label();
+            name_oma_lbl = new Label();
             oma_tel_txt = new TextBox();
-            label3 = new Label();
-            label4 = new Label();
+            Telefon_oma_lbl = new Label();
             lisa_oma_btn = new Button();
             puhasta_oma_btn = new Button();
-            toolTip_oma = new ToolTip(components);
+            toolTip = new ToolTip(components);
+            dataGridView_autod = new DataGridView();
+            Autod_lbl = new Label();
+            Mudel_auto_lbl = new Label();
+            RN_auto_lbl = new Label();
+            OmaId_auto_lbl = new Label();
+            Brand_auto_lbl = new Label();
+            OmaId_auto_cb = new ComboBox();
+            RN_auto_txt = new TextBox();
+            mudel_auto_txt = new TextBox();
+            brand_auto_txt = new TextBox();
+            lisa_auto_btn = new Button();
+            puhasta_auto_btn = new Button();
             tabC.SuspendLayout();
             Omanik.SuspendLayout();
+            Autod.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView_omanik).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView_autod).BeginInit();
             SuspendLayout();
             // 
             // tabC
@@ -61,6 +77,7 @@
             tabC.SelectedIndex = 0;
             tabC.Size = new Size(356, 233);
             tabC.TabIndex = 0;
+            tabC.Click += tabC_Click;
             // 
             // Omanik
             // 
@@ -74,7 +91,6 @@
             Omanik.TabIndex = 0;
             Omanik.Text = "Omanikud";
             Omanik.UseVisualStyleBackColor = true;
-            Omanik.Click += Omanik_Click;
             // 
             // Kustuta_oma_btn
             // 
@@ -114,6 +130,9 @@
             // 
             // Autod
             // 
+            Autod.Controls.Add(Kust_auto_btm);
+            Autod.Controls.Add(Lisavorm_auto_btn);
+            Autod.Controls.Add(Vaata_auto_btn);
             Autod.Location = new Point(4, 24);
             Autod.Name = "Autod";
             Autod.Padding = new Padding(3);
@@ -121,7 +140,42 @@
             Autod.TabIndex = 1;
             Autod.Text = "Autod";
             Autod.UseVisualStyleBackColor = true;
-            Autod.Click += Autod_Click;
+            // 
+            // Kust_auto_btm
+            // 
+            Kust_auto_btm.BackColor = Color.Gray;
+            Kust_auto_btm.ForeColor = SystemColors.ControlLightLight;
+            Kust_auto_btm.Location = new Point(51, 151);
+            Kust_auto_btm.Name = "Kust_auto_btm";
+            Kust_auto_btm.Size = new Size(255, 35);
+            Kust_auto_btm.TabIndex = 3;
+            Kust_auto_btm.Text = "Kustuta";
+            Kust_auto_btm.UseVisualStyleBackColor = false;
+            Kust_auto_btm.Click += Kust_auto_btm_Click;
+            // 
+            // Lisavorm_auto_btn
+            // 
+            Lisavorm_auto_btn.BackColor = Color.Gray;
+            Lisavorm_auto_btn.ForeColor = SystemColors.ControlLight;
+            Lisavorm_auto_btn.Location = new Point(51, 85);
+            Lisavorm_auto_btn.Name = "Lisavorm_auto_btn";
+            Lisavorm_auto_btn.Size = new Size(255, 35);
+            Lisavorm_auto_btn.TabIndex = 2;
+            Lisavorm_auto_btn.Text = "Lisavorm";
+            Lisavorm_auto_btn.UseVisualStyleBackColor = false;
+            Lisavorm_auto_btn.Click += Lisavorm_auto_btn_Click;
+            // 
+            // Vaata_auto_btn
+            // 
+            Vaata_auto_btn.BackColor = Color.Gray;
+            Vaata_auto_btn.ForeColor = SystemColors.ControlLightLight;
+            Vaata_auto_btn.Location = new Point(51, 17);
+            Vaata_auto_btn.Name = "Vaata_auto_btn";
+            Vaata_auto_btn.Size = new Size(255, 35);
+            Vaata_auto_btn.TabIndex = 1;
+            Vaata_auto_btn.Text = "Vaata";
+            Vaata_auto_btn.UseVisualStyleBackColor = false;
+            Vaata_auto_btn.Click += Vaata_auto_btn_Click;
             // 
             // HjT
             // 
@@ -131,7 +185,6 @@
             HjT.TabIndex = 2;
             HjT.Text = "Hooldus ja Teenused";
             HjT.UseVisualStyleBackColor = true;
-            HjT.Click += HjT_Click;
             // 
             // dataGridView_omanik
             // 
@@ -150,47 +203,39 @@
             Omanikud.TabIndex = 1;
             Omanikud.Text = "Omanikud";
             // 
-            // oma_nimi_box
+            // oma_nimi_txt
             // 
             oma_nimi_txt.Location = new Point(205, 110);
-            oma_nimi_txt.Name = "oma_nimi_box";
+            oma_nimi_txt.Name = "oma_nimi_txt";
             oma_nimi_txt.Size = new Size(120, 23);
             oma_nimi_txt.TabIndex = 2;
             // 
-            // label2
+            // name_oma_lbl
             // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 186);
-            label2.Location = new Point(12, 110);
-            label2.Name = "label2";
-            label2.Size = new Size(121, 21);
-            label2.TabIndex = 3;
-            label2.Text = "Ees- ja perenimi";
+            name_oma_lbl.AutoSize = true;
+            name_oma_lbl.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 186);
+            name_oma_lbl.Location = new Point(12, 110);
+            name_oma_lbl.Name = "name_oma_lbl";
+            name_oma_lbl.Size = new Size(121, 21);
+            name_oma_lbl.TabIndex = 3;
+            name_oma_lbl.Text = "Ees- ja perenimi";
             // 
-            // oma_tel_box
+            // oma_tel_txt
             // 
             oma_tel_txt.Location = new Point(205, 169);
-            oma_tel_txt.Name = "oma_tel_box";
+            oma_tel_txt.Name = "oma_tel_txt";
             oma_tel_txt.Size = new Size(120, 23);
             oma_tel_txt.TabIndex = 4;
             // 
-            // label3
+            // Telefon_oma_lbl
             // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 186);
-            label3.Location = new Point(12, 171);
-            label3.Name = "label3";
-            label3.Size = new Size(59, 21);
-            label3.TabIndex = 5;
-            label3.Text = "Telefon";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(44, 246);
-            label4.Name = "label4";
-            label4.Size = new Size(0, 15);
-            label4.TabIndex = 6;
+            Telefon_oma_lbl.AutoSize = true;
+            Telefon_oma_lbl.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 186);
+            Telefon_oma_lbl.Location = new Point(12, 171);
+            Telefon_oma_lbl.Name = "Telefon_oma_lbl";
+            Telefon_oma_lbl.Size = new Size(59, 21);
+            Telefon_oma_lbl.TabIndex = 5;
+            Telefon_oma_lbl.Text = "Telefon";
             // 
             // lisa_oma_btn
             // 
@@ -212,6 +257,113 @@
             puhasta_oma_btn.UseVisualStyleBackColor = true;
             puhasta_oma_btn.Click += puhasta_oma_Click;
             // 
+            // dataGridView_autod
+            // 
+            dataGridView_autod.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView_autod.Location = new Point(13, 265);
+            dataGridView_autod.Name = "dataGridView_autod";
+            dataGridView_autod.Size = new Size(802, 277);
+            dataGridView_autod.TabIndex = 1;
+            // 
+            // Autod_lbl
+            // 
+            Autod_lbl.AutoSize = true;
+            Autod_lbl.Font = new Font("Bernard MT Condensed", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Autod_lbl.Location = new Point(31, 12);
+            Autod_lbl.Name = "Autod_lbl";
+            Autod_lbl.Size = new Size(101, 44);
+            Autod_lbl.TabIndex = 2;
+            Autod_lbl.Text = "Autod";
+            // 
+            // Mudel_auto_lbl
+            // 
+            Mudel_auto_lbl.AutoSize = true;
+            Mudel_auto_lbl.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 186);
+            Mudel_auto_lbl.Location = new Point(31, 126);
+            Mudel_auto_lbl.Name = "Mudel_auto_lbl";
+            Mudel_auto_lbl.Size = new Size(54, 21);
+            Mudel_auto_lbl.TabIndex = 3;
+            Mudel_auto_lbl.Text = "Mudel";
+            // 
+            // RN_auto_lbl
+            // 
+            RN_auto_lbl.AutoSize = true;
+            RN_auto_lbl.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 186);
+            RN_auto_lbl.Location = new Point(31, 176);
+            RN_auto_lbl.Name = "RN_auto_lbl";
+            RN_auto_lbl.Size = new Size(165, 21);
+            RN_auto_lbl.TabIndex = 4;
+            RN_auto_lbl.Text = "Registreerimisnumber";
+            // 
+            // OmaId_auto_lbl
+            // 
+            OmaId_auto_lbl.AutoSize = true;
+            OmaId_auto_lbl.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 186);
+            OmaId_auto_lbl.Location = new Point(31, 220);
+            OmaId_auto_lbl.Name = "OmaId_auto_lbl";
+            OmaId_auto_lbl.Size = new Size(65, 21);
+            OmaId_auto_lbl.TabIndex = 5;
+            OmaId_auto_lbl.Text = "Omanik";
+            // 
+            // Brand_auto_lbl
+            // 
+            Brand_auto_lbl.AutoSize = true;
+            Brand_auto_lbl.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 186);
+            Brand_auto_lbl.Location = new Point(31, 82);
+            Brand_auto_lbl.Name = "Brand_auto_lbl";
+            Brand_auto_lbl.Size = new Size(51, 21);
+            Brand_auto_lbl.TabIndex = 6;
+            Brand_auto_lbl.Text = "Bränd";
+            // 
+            // OmaId_auto_cb
+            // 
+            OmaId_auto_cb.FormattingEnabled = true;
+            OmaId_auto_cb.Location = new Point(219, 218);
+            OmaId_auto_cb.Name = "OmaId_auto_cb";
+            OmaId_auto_cb.Size = new Size(130, 23);
+            OmaId_auto_cb.TabIndex = 7;
+            // 
+            // RN_auto_txt
+            // 
+            RN_auto_txt.Location = new Point(219, 174);
+            RN_auto_txt.Name = "RN_auto_txt";
+            RN_auto_txt.Size = new Size(130, 23);
+            RN_auto_txt.TabIndex = 8;
+            // 
+            // mudel_auto_txt
+            // 
+            mudel_auto_txt.Location = new Point(219, 124);
+            mudel_auto_txt.Name = "mudel_auto_txt";
+            mudel_auto_txt.Size = new Size(130, 23);
+            mudel_auto_txt.TabIndex = 9;
+            // 
+            // brand_auto_txt
+            // 
+            brand_auto_txt.Location = new Point(219, 80);
+            brand_auto_txt.Name = "brand_auto_txt";
+            brand_auto_txt.Size = new Size(130, 23);
+            brand_auto_txt.TabIndex = 10;
+            // 
+            // lisa_auto_btn
+            // 
+            lisa_auto_btn.Location = new Point(101, 221);
+            lisa_auto_btn.Name = "lisa_auto_btn";
+            lisa_auto_btn.Size = new Size(65, 24);
+            lisa_auto_btn.TabIndex = 7;
+            lisa_auto_btn.Text = "Lisa";
+            lisa_auto_btn.UseVisualStyleBackColor = true;
+            lisa_auto_btn.Click += Lisa_oma_Click;
+            // 
+            // puhasta_auto_btn
+            // 
+            puhasta_auto_btn.Location = new Point(178, 221);
+            puhasta_auto_btn.Name = "puhasta_auto_btn";
+            puhasta_auto_btn.Size = new Size(65, 24);
+            puhasta_auto_btn.TabIndex = 8;
+            puhasta_auto_btn.Text = "Puhasta";
+            puhasta_auto_btn.UseVisualStyleBackColor = true;
+            puhasta_auto_btn.Click += puhasta_oma_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -223,7 +375,9 @@
             Load += Form1_Load;
             tabC.ResumeLayout(false);
             Omanik.ResumeLayout(false);
+            Autod.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)dataGridView_omanik).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView_autod).EndInit();
             ResumeLayout(false);
 
         }
@@ -240,12 +394,26 @@
         private DataGridView dataGridView_omanik;
         private Label Omanikud;
         private TextBox oma_nimi_txt;
-        private Label label2;
+        private Label name_oma_lbl;
         private TextBox oma_tel_txt;
-        private Label label3;
-        private Label label4;
+        private Label Telefon_oma_lbl;
         private Button lisa_oma_btn;
         private Button puhasta_oma_btn;
-        private ToolTip toolTip_oma;
+        private ToolTip toolTip;
+        private Button Kust_auto_btm;
+        private Button Lisavorm_auto_btn;
+        private Button Vaata_auto_btn;
+        private DataGridView dataGridView_autod;
+        private Label Autod_lbl;
+        private Label Mudel_auto_lbl;
+        private Label RN_auto_lbl;
+        private Label OmaId_auto_lbl;
+        private Label Brand_auto_lbl;
+        private ComboBox OmaId_auto_cb;
+        private TextBox RN_auto_txt;
+        private TextBox mudel_auto_txt;
+        private TextBox brand_auto_txt;
+        private Button lisa_auto_btn;
+        private Button puhasta_auto_btn;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -11,112 +11,34 @@ namespace Autod
         {
             _db = new AutoDbContext();
             InitializeComponent();
+            LaeOmanik();
             LaeAutod();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             Kustuta_oma_btn.Enabled = false;
+            toolTip.SetToolTip(Kustuta_oma_btn, "Tabelist andmete kustutamiseks valige kustutatavad isikud.");
             Kustuta_oma_btn.ForeColor = Color.Silver;
-            toolTip_oma.SetToolTip(Kustuta_oma_btn, "Tabelist andmete kustutamiseks valige kustutatavad isikud.");
+
+            Kust_auto_btm.Enabled = false;
+            Kust_auto_btm.ForeColor = Color.Silver;
+            toolTip.SetToolTip(Kust_auto_btm, "Tabelist andmete kustutamiseks valige kustutatavad isikud.");
         }
-        private void LaeAutod()
+        private void tabC_Click(object sender, EventArgs e)
         {
-            dataGridView_omanik.DataSource = _db.Omanik.ToList();
+            Controls.Remove(dataGridView_omanik);
+            Controls.Remove(puhasta_oma_btn);
+            Controls.Remove(lisa_oma_btn);
+            Controls.Remove(name_oma_lbl);
+            Controls.Remove(oma_tel_txt);
+            Controls.Remove(Telefon_oma_lbl);
+            Controls.Remove(oma_nimi_txt);
+            Controls.Remove(Omanikud);
 
-        }
-        private void HjT_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void Omanik_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void Autod_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Kustuta_Click(object sender, EventArgs e)
-        {
-            if (dataGridViewTooded.SelectedRows.Count == 0)
-            {
-                MessageBox.Show("Palun valige kasutatav toode.", "Viga", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-            string tooteNimetus = dataGridViewTooded.SelectedRows[0].Cells["Toodenimetus"].Value?.ToString() ?? "valitud toode";
-            DialogResult vastus = MessageBox.Show(
-                $"Kas olete kindel, et soovite kustuta toote: {tooteNimetus}?",
-                "Kustutamise kinnitus",
-                MessageBoxButtons.YesNo,
-                MessageBoxIcon.Question
-                );
-            if (vastus == DialogResult.Yes)
-            {
-                try
-                {
-                    int id = (int)dataGridViewTooded.SelectedRows[0].Cells["Id"].Value;
-                    var toode = _db.Tootetabel.Find(id);
-
-                    if (toode != null)
-                    {
-                        _db.Tootetabel.Remove(toode);
-                        _db.SaveChanges();
-                        LaeTooted();
-                        PuhastaVorm();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Kustutamisel tekkis viga: {ex.Message}", "Viga", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-        }
-
-        private void Lisa_Click(object sender, EventArgs e)
-        {
-            Controls.Add(puhasta_oma_btn);
-            Controls.Add(lisa_oma_btn);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(oma_tel_txt);
-            Controls.Add(label2);
-            Controls.Add(oma_nimi_txt);
-            Controls.Add(Omanikud);
-        }
-
-        private void Lisa_oma_Click(object sender, EventArgs e)
-        {
-
-            if (string.IsNullOrWhiteSpace(oma_nimi_txt.Text))
-            {
-                MessageBox.Show("Toode nimetus on kohustuslik");
-                return;
-            }
-            var uus = new Owner
-            {
-                FullName = oma_nimi_txt.Text,
-                Phone = oma_tel_txt.Text
-            };
-            _db.Omanik.Add(uus);
-            _db.SaveChanges();
-            LaeAutod();
-            Controls.Add(dataGridView_omanik);
-            Kustuta_oma_btn.Enabled = true;
-            Kustuta_oma_btn.ForeColor = Color.White;
-        }
-        private void puhasta_oma_Click(object sender, EventArgs e)
-        {
-            oma_nimi_txt.Text = "";
-            oma_tel_txt.Text = "";
-        }
-        private void Vaata_Click(object sender, EventArgs e)
-        {
-            Kustuta_oma_btn.Enabled = true;
-            Kustuta_oma_btn.ForeColor = Color.White;
-
-            Controls.Add(dataGridView_omanik);
+            Kust_auto_btm.Enabled = false;
+            Kust_auto_btm.ForeColor = Color.Silver;
+            Kustuta_oma_btn.Enabled = false;
+            Kustuta_oma_btn.ForeColor = Color.Silver;
         }
     }
 }
